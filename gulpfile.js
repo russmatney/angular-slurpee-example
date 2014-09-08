ec662d8a2fdcd66ec736b342b3a66c75dd8e9036
Russell Matney <russell.matney@gmail.com>
2014-09-08 00:07:40 -0400

    updates to latest slurpee, fixes config names

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ var slurpee = require('slurpee');
 
 slurpee.config.cssFile = 'app.css';
 slurpee.config.indexFile = 'index.html';
-slurpee.config.jsFile = 'scripts/controller.js';
+slurpee.config.jsFile = 'app.js';
 slurpee.config.jsPaths = ['scripts/*.js'];
 slurpee.config.jsRootPath = './js';
 slurpee.config.liveReloadPort = 35729;
-slurpee.config.outputDir = './';
-slurpee.config.staticPath = undefined;
+slurpee.config.outputDir = 'public/';
+slurpee.config.staticDir = 'public';
 slurpee.config.staticPort = 3000;
-slurpee.config.useBower = false;
+slurpee.config.useBower = true;
 
 slurpee.configure();
 
 var gulp = require('gulp');
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'serve', 'watch']);
